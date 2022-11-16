e4315e7d39d9b6bba105fce6cdbd5892f039f28b
Zachery Schulwolf <zacwolf3@gmail.com>
2022-11-16 13:32:12 -0600

    Messing with ipynbs, tried to get pctchange to work

--- exp/exp_basic.py
+++ exp/exp_basic.py
@@ -15,8 +15,8 @@ class Exp_Basic(object):
     def _acquire_device(self):
         if self.args.use_gpu:
             os.environ["CUDA_VISIBLE_DEVICES"] = str(self.args.gpu) if not self.args.use_multi_gpu else self.args.devices
-            device = torch.device('cuda:{}'.format(self.args.gpu))
-            print('Use GPU: cuda:{}'.format(self.args.gpu))
+            device = torch.device(f"cuda:{self.args.gpu}")
+            print(f"Use GPU: cuda:{self.args.gpu}")
         else:
             device = torch.device('cpu')
             print('Use CPU')
